19ae40e5ad5ac8f4565282e7db872bbbba6431b6
Himanshu <hp200229@gmail.com>
2021-04-25 12:40:48 +0530

    Added Modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,46 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/card.css'
 import star from "../assets/icons/star-solid.svg";
+import Modal from './Modal'
 
 
 const API_KEY = "269385d2";
 
-const getDetails = async (title) => {
-  const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`
-  );
+// const getDetails = async (title) => {
+//   const response = await fetch(
+//     `https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`
+//   );
 
-  const data = await response.json();
-  console.log("Details", data);
-};
+//   const data = await response.json();
+//   console.log("Details", data);
+//   return data;
+// };
 
-const getReviews = async (id) => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=4c397752e5cefdf86d36ee3e05ff8a23&language=en-US&page=1`
-  );
-  const data = await response.json();
-  console.log("Reviews", data.results)
-}
+// const getReviews = async (id) => {
+//   const response = await fetch(
+//     `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=4c397752e5cefdf86d36ee3e05ff8a23&language=en-US&page=1`
+//   );
+//   const data = await response.json();
+//   console.log("Reviews", data.results)
+//   return data.results;
+// }
 
-const getSimilar = async (id) => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}/similar?api_key=4c397752e5cefdf86d36ee3e05ff8a23&language=en-US&page=1`
-  );
-  const data = await response.json();
-  console.log("Similar",data.results);
-}
+// const getSimilar = async (id) => {
+//   const response = await fetch(
+//     `https://api.themoviedb.org/3/movie/${id}/similar?api_key=4c397752e5cefdf86d36ee3e05ff8a23&language=en-US&page=1`
+//   );
+//   const data = await response.json();
+//   console.log("Similar",data.results);
+//   return data.results;
+// }
 
-const handleClick = () => {
-
-}
 
 export function ExtendedCard({ img_src, overview, title, rating, release_date, id}) {
    if (img_src == null || title == null || overview == null) return null;
   return (
     <div className="extended-card" onClick={() =>{
-      getDetails(title);
-      getReviews(id);
-      getSimilar(id);
+      // getDetails(title);
+      // getReviews(id);
+      // getSimilar(id);
     }}>
       <div className="image">
         <img src={`https://image.tmdb.org/t/p/w500${img_src}`} alt="" />
@@ -59,13 +60,58 @@ export function ExtendedCard({ img_src, overview, title, rating, release_date, i
 }
 
 export function Card({ img_src, title, rating, id}) {
+
+  const [details, setDetails] = useState([]);
+  const [similar, setSimilar] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
+
+  const getDetails = async (title) => {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`
+    );
+
+    const data = await response.json();
+    console.log("Details", data);
+    setDetails(data)
+  };
+
+  const getReviews = async (id) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=4c397752e5cefdf86d36ee3e05ff8a23&language=en-US&page=1`
+    );
+    const data = await response.json();
+    console.log("Reviews", data.results);
+    // return data.results;
+    setReviews(data.results)
+  };
+
+  const getSimilar = async (id) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/similar?api_key=4c397752e5cefdf86d36ee3e05ff8a23&language=en-US&page=1`
+    );
+    const data = await response.json();
+    console.log("Similar", data.results);
+    // return data.results;
+    setSimilar(data.results)
+  };
+
+  const [hide, setHide] = useState(true)
+
+  const showModal = () => setHide(false);
+  const hideModal = () => setHide(true);
+
   if (img_src == null || title == null) return null;
   return (
-    <div className="movie" onClick={() =>{
-      getDetails(title);
-      getReviews(id);
-      getSimilar(id);
-    }}>
+    <div
+      className="movie"
+      onClick={() => {
+        getDetails(title);
+        getReviews(id);
+        getSimilar(id);
+        showModal();
+      }}
+    >
       <img
         src={`https://image.tmdb.org/t/p/w500${img_src}`}
         alt=""
@@ -75,6 +121,25 @@ export function Card({ img_src, title, rating, id}) {
       <h4 className="rating">
         <img src={star} alt="" className="star" /> {rating}
       </h4>
+      <div className={hide ? "hide" : "cont"} onClick={hideModal}>
+        <Modal
+          title={details.Title}
+          release_year={details.Released}
+          runtime={details.Runtime}
+          genre={details.Genre}
+          director={details.Director}
+          actors={details.Actors}
+          lang={details.Language}
+          country={details.Country}
+          awards={details.Awards}
+          prod={details.Production}
+          plot={details.Plot}
+          ratings={details.Ratings}
+          image={img_src}
+          similars={similar}
+          reviews={reviews}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Review from './Review'
-import Card from './Card'
+import {Card} from './Card'
 import '../css/Modal.css'
 import { act } from 'react-dom/test-utils';
 
@@ -21,37 +21,69 @@ function Modal({
   similars,
   reviews,
 }) {
+
+  const [hide, sethide] = useState(false)
+  const Hidemodal = () => sethide(true)
   return (
-    <div className="modal">
+    <div className={hide ? "hide" : "modal"}>
+      <div className="close" onClick={Hidemodal}>+</div>
       <h1 className="title">{title}</h1>
       <div className="details-section">
         <div className="image-section">
-          <img src={image} alt="" className="poster" />
-          <p className="rating">IMDb: {ratings[0].value}</p>
-          <p className="rating">Rotten Tomatoes: {ratings[1].value}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${image}`}
+            alt=""
+            className="poster"
+          />
+          {/* <p className="rating">IMDb: {ratings[0].value}</p> */}
+          {/* <p className="rating">Rotten Tomatoes: {ratings[1].value}</p> */}
         </div>
         <div className="details">
-          <p>Year: {release_year}</p>
-          <p>Runtime: {runtime}</p>
-          <p>Genre: {genre}</p>
-          <p>Director: {director}</p>
-          <p>Actors: {actors}</p>
-          <p>Language: {lang}</p>
-          <p>Country: {country}</p>
-          <p>Awards: {awards}</p>
-          <p>Production: {prod}</p>
-          <p>Plot: {plot}</p>
+          <p>
+            <span>Released:</span> {release_year}
+          </p>
+          <p>
+            <span>Runtime:</span> {runtime}
+          </p>
+          <p>
+            <span>Genre:</span> {genre}
+          </p>
+          <p>
+            <span>Director:</span> {director}
+          </p>
+          <p>
+            <span>Actors:</span> {actors}
+          </p>
+          <p>
+            <span>Language:</span> {lang}
+          </p>
+          <p>
+            <span>Country:</span> {country}
+          </p>
+          <p>
+            <span>Awards:</span> {awards}
+          </p>
+          <p>
+            <span>Production:</span> {prod}
+          </p>
+          <p>
+            <span>Plot:</span> {plot}
+          </p>
         </div>
       </div>
       <div className="similar">
-        {similars.map((movie) => (
-          <Card
-            img_src={movie.poster_path}
-            title={movie.title}
-            rating={movie.vote_average}
-            id={movie.id}
-          />
-        ))}
+        <h2 className="heading">Similar Movies</h2>
+        <div className="content">
+         
+          {similars.map((movie) => (
+            <Card
+              img_src={movie.poster_path}
+              title={movie.title}
+              rating={movie.vote_average}
+              id={movie.id}
+            />
+          ))}
+        </div>
       </div>
       <div className="reviews">
         {reviews.map((review) => (
